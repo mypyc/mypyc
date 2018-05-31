f6137d9bdc6a1f75ab4e7456b058f98d30a3c59d
Jukka Lehtosalo <jukka@dropbox.com>
2018-05-31 16:32:05 +0100

    More refactoring

--- mypyc/ops_tuple.py
+++ mypyc/ops_tuple.py
@@ -10,18 +10,15 @@ from mypyc.ops import (
     EmitterInterface, PrimitiveOp, tuple_rprimitive, int_rprimitive, list_rprimitive,
     object_rprimitive, ERR_NEVER, ERR_MAGIC
 )
-from mypyc.ops_primitive import method_op, func_op
+from mypyc.ops_primitive import method_op, func_op, simple_emit
 
 
-def emit_get_item(emitter: EmitterInterface, args: List[str], dest: str) -> None:
-    emitter.emit_line('%s = CPySequenceTuple_GetItem(%s, %s);' % (dest, args[0], args[1]))
-
-
-tuple_get_item_op = method_op(name='builtins.tuple.__getitem__',
-                              arg_types=[tuple_rprimitive, int_rprimitive],
-                              result_type=object_rprimitive,
-                              error_kind=ERR_MAGIC,
-                              emit=emit_get_item)
+tuple_get_item_op = method_op(
+    name='builtins.tuple.__getitem__',
+    arg_types=[tuple_rprimitive, int_rprimitive],
+    result_type=object_rprimitive,
+    error_kind=ERR_MAGIC,
+    emit=simple_emit('{dest} = CPySequenceTuple_GetItem({args[0]}, {args[1]});'))
 
 
 def emit_len(emitter: EmitterInterface, args: List[str], dest: str) -> None:
@@ -31,19 +28,17 @@ def emit_len(emitter: EmitterInterface, args: List[str], dest: str) -> None:
     emitter.emit_line('%s = CPyTagged_ShortFromLongLong(%s);' % (dest, temp))
 
 
-tuple_len_op = func_op(name='builtins.len',
-                       arg_types=[tuple_rprimitive],
-                       result_type=int_rprimitive,
-                       error_kind=ERR_NEVER,
-                       emit=emit_len)
+tuple_len_op = func_op(
+    name='builtins.len',
+    arg_types=[tuple_rprimitive],
+    result_type=int_rprimitive,
+    error_kind=ERR_NEVER,
+    emit=emit_len)
 
 
-def emit_from_list(emitter: EmitterInterface, args: List[str], dest: str) -> None:
-    emitter.emit_line('%s = PyList_AsTuple(%s);' % (dest, args[0]))
-
-
-list_tuple_op = func_op(name='builtins.tuple',
-                        arg_types=[list_rprimitive],
-                        result_type=tuple_rprimitive,
-                        error_kind=ERR_MAGIC,
-                        emit=emit_from_list)
+list_tuple_op = func_op(
+    name='builtins.tuple',
+    arg_types=[list_rprimitive],
+    result_type=tuple_rprimitive,
+    error_kind=ERR_MAGIC,
+    emit=simple_emit('{dest} = PyList_AsTuple({args[0]});'))
