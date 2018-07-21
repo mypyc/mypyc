ae64716e3f8f839cd4726625ff646b044996a11b
Adam Smith <adamsmith@dropbox.com>
2018-07-20 17:56:15 -0700

    add the in place ops

--- mypyc/ops_misc.py
+++ mypyc/ops_misc.py
@@ -101,6 +101,25 @@ for op, funcname in [('+=', 'PyNumber_InPlaceAdd'),
               emit=simple_emit('{dest} = %s({args[0]}, {args[1]});' % funcname),
               priority=0)
 
+for op, funcname in [('+=', 'PyNumber_InPlaceAdd'),
+                     ('-=', 'PyNumber_Subtract'),
+                     ('*=', 'PyNumber_InPlaceMultiply'),
+                     ('@=', 'PyNumber_InPlaceMatrixMultiply'),
+                     ('//=', 'PyNumber_InPlaceFloorDivide'),
+                     ('/=', 'PyNumber_InPlaceTrueDivide'),
+                     ('%=', 'PyNumber_InPlaceRemainder'),
+                     ('<<=', 'PyNumber_InPlaceLshift'),
+                     ('>>=', 'PyNumber_InPlaceRshift'),
+                     ('&=', 'PyNumber_InPlaceAnd'),
+                     ('^=', 'PyNumber_InPlaceXor'),
+                     ('|=', 'PyNumber_InPlaceOr')]:
+    binary_op(op=op,
+              arg_types=[object_rprimitive, object_rprimitive],
+              result_type=object_rprimitive,
+              error_kind=ERR_MAGIC,
+              emit=simple_emit('{dest} = %s({args[0]}, {args[1]});' % funcname),
+              priority=0)
+
 binary_op(op='**',
           arg_types=[object_rprimitive, object_rprimitive],
           result_type=object_rprimitive,
