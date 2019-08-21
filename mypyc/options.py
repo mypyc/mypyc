da217f4d95bb84d93fbfd7f4fa9e6c66f3cc5435
Michael J. Sullivan <sully@msully.net>
2019-08-21 14:36:03 -0700

    Complete the move into mypy!

--- mypyc/options.py
+++
@@ -1,6 +0,0 @@
-class CompilerOptions:
-    def __init__(self, strip_asserts: bool = False, multi_file: bool = False,
-                 verbose: bool = False) -> None:
-        self.strip_asserts = strip_asserts
-        self.multi_file = multi_file
-        self.verbose = verbose
